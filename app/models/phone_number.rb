0f7a3e1d1b71c0457727fb635b73b150ff71ff58
Teresa Finn <teresa.finn@gmail.com>
2015-03-01 13:35:51 -0700

    Add validation to phone number model

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
+
+  validates :number, presence: true
+
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:phone_number) { PhoneNumber.new(number: '2024600772') }
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+
+  it 'is invalid without a number' do
+    phone_number.number = nil
+    expect(phone_number).to_not be_valid
+  end
+
 end
